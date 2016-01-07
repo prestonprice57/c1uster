0e2dcafb991238c79a8d165504bc731a80b887aa
Jax Davis <jaxdavis@Jaxs-MacBook-Pro.local>
2016-01-07 18:59:53 -0500

    Added submit button linked to function

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,10 @@
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
 
-var formApp = angular.module('customerApp', [])
+var customerApp = angular.module('customerApp', [])
     .controller('customerController', function($scope) {
-        $scope.customerData = {}; 
+        $scope.customerData = {"customerInfo":{"gender":false,"income":false,"age":false}}
+
+        $scope.submit = function() {
+              console.log($scope.customerData.customerInfo);
+            };
     });
